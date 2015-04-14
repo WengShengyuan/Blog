1064fc7893a4df01ebff5c9128aaafba4f52bc20
6655449987 <6655449987qq@gmail.com>
2015-04-14 22:37:07 +0800

    #

--- javascripts/back_to_top.js
+++ javascripts/back_to_top.js
@@ -1,14 +1,18 @@
-$("#back-top").hide();
-$(document).ready(function() {
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 100) {
-			$('#back-top').fadeIn();
-		} else {
-			$('#back-top').fadeOut();
-		}
+$(document).ready(function(){ 
+	$("#back-top").hide();
+	$(document).ready(function() {
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+		$('#back-top a').click(function() {
+			$('html,body').animate({scrollTop : 0}, 800);
+			return false;
+		});
 	});
-	$('#back-top a').click(function() {
-		$('html,body').animate({scrollTop : 0}, 800);
-		return false;
-	});
-});
+
+}
+
