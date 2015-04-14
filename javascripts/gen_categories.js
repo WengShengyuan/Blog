5cce11906de5b288f3bd0318ceac3f50d5fa7065
6655449987 <6655449987qq@gmail.com>
2015-04-15 07:32:43 +0800

    #

--- javascripts/gen_categories.js
+++ javascripts/gen_categories.js
@@ -1,9 +1,12 @@
 
 	$(document).ready(function() {
-		var dataStr = '{ {% for tag in site.tags %}{% if tag[0] != site.tags.first[0] %},{% endif %}"{{ tag[0] }}":[{% for post in tag[1] %}{% if post != tag[1].first %},{% endif %}{"url":"{{post.url}}", "title":"{{post.title}}", "date":"{{post.date | date:"%d/%m/%Y"}}"}{% endfor %}]{% endfor %} }',
-	    data = JSON.parse(dataStr),
-	    curTag = $.query.get("tag"),
-	    archieves = data[curTag];
-		console.log(archieves);
+		var cat = getQueryString("cat");
+		alert(cat);
 	});
+	
+	function getQueryString(name) {
+	    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+	    var r = window.location.search.substr(1).match(reg);
+	    if (r != null) return unescape(r[2]); return null;
+	    }
 
