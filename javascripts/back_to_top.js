cc2b90b27c2b54f1c9080e252a0529c43cb7a0c5
6655449987 <6655449987qq@gmail.com>
2015-04-14 22:38:31 +0800

    #

--- javascripts/back_to_top.js
+++ javascripts/back_to_top.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){ 
-	alert('11');
-	$("#back-top").hide();
+
+
 	$(document).ready(function() {
+		$("#back-top").hide();
 		$(window).scroll(function() {
 			if ($(this).scrollTop() > 100) {
 				$('#back-top').fadeIn();
@@ -15,5 +15,4 @@ $(document).ready(function(){
 		});
 	});
 
-}
 
